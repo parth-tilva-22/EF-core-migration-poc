292df04c3472b23d0fb10fd11de80baa9f5f20f3
MRISOFTWARE\Parth.Tilva <Parth.Tilva@mrisoftware.com>
2024-03-27 18:44:26 +0530

    Initial commit

---
+++ EF-core-migration-poc/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EF_core_migration_poc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_core_migration_poc.Data {
+  public class AppDbContext : DbContext {
+    public AppDbContext(DbContextOptions options) : base(options) {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+  }
+}
---
+++ EF-core-migration-poc/Models/Employee.cs
@@ -0,0 +1,6 @@
+﻿namespace EF_core_migration_poc.Models {
+  public class Employee {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+  }
+}
